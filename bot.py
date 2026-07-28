8a5b6de6955ad2bd6a26a987430d58d7c2092f38
墨年&的梦 <110153229+bigmn666@users.noreply.github.com>
2026-07-29 01:21:20 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,34 +1,28 @@
+import os
 import requests
 
 def check_price():
-    # 1. 请求 Dexscreener 获取价格数据
     url = "https://api.dexscreener.com/latest/dex/pairs/bsc/0x5a4bbfa871f6cacb80cfffcb04d63f6366c7cb5f"
     try:
         response = requests.get(url, timeout=10)
         data = response.json()
         
-        # 获取价格（对应你原本的 pairs[1].priceUsd）
         pairs = data.get("pairs", [])
         if len(pairs) > 1:
             price_usd = float(pairs[1]["priceUsd"])
         else:
-            price_usd = float(pairs[0]["priceUsd"]) # 防止数组越界兜底
+            price_usd = float(pairs[0]["priceUsd"])
             
         print(f"当前获取到价格: {price_usd} USD")
         
-        # 2. 条件判断与微信推送逻辑
-        # 对应你原本路由中的判断条件
-        # 3D 价格条件判断与微信推送逻辑
         if price_usd > 100:
-            # 大于 100 时的提示
             send_wechat(
-                f"🚨 价格预警通知（高位突破）\n\n当前代币价格已升至为：{price_usd} USD，本消息来自mac-docker", 
+                f"🚨 价格预警通知（高位突破）\n\n当前代币价格已升至为：{price_usd} USD，本消息来自GitHub Actions", 
                 f"🚨🚨🚨 当前价格:{price_usd}🚨价格升至 100 以上🚨🚨🚨"
             )
-        elif price_usd < 40:
-            # 小于 40 时的提示
+        elif price_usd < 50:
             send_wechat(
-                f"⚠️ 价格预警通知（跌破警戒线）\n\n当前代币价格已跌至：{price_usd} USD本消息来自mac-docker", 
+                f"⚠️ 价格预警通知（跌破警戒线）\n\n当前代币价格已跌至：{price_usd} USD本消息来自GitHub Actions", 
                 f"⚠️⚠️⚠️ 当前价格: {price_usd} 跌至 40 以下 ⚠️⚠️⚠️"
             )
             
@@ -36,17 +30,19 @@ def check_price():
         print(f"请求出错: {e}")
 
 def send_wechat(content, summary):
-    # WxPusher 推送接口
     wx_url = "https://wxpusher.zjiecode.com/api/send/message"
+    
+    # 对应你刚才在 GitHub 里添加的三个 Secret 名字
+    app_token = os.getenv("WXPUSHER_TOKEN")
+    uid_1 = os.getenv("WX_UID_1")
+    uid_2 = os.getenv("WX_UID_2")
+
     payload = {
-        "appToken": "AT_hylyDyyClQYRQAiPqlZgN6H5bWv6txJX",
+        "appToken": app_token,
         "content": content,
         "summary": summary,
         "contentType": 1,
-        "uids": [
-            "UID_SkGWz0MxDpsC7x83MEknBFx8GXzk",
-            "UID_Le64zRgq2tEO64GQgkkMiK5A0mWP"
-        ]
+        "uids": [uid_1, uid_2]
     }
     res = requests.post(wx_url, json=payload)
     print("微信推送结果:", res.text)
